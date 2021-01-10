2c554ffc2e63ebd735a1e33dd455063ff5138e9d
Vivekjvs <vivekjandhyala@gmail.com>
2021-01-10 23:35:43 +0530

    BUILD v-0.1.2

--- app/src/main/java/com/aai_project/inventory/EquipmentGen.kt
+++ app/src/main/java/com/aai_project/inventory/EquipmentGen.kt
@@ -5,10 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.aai_project.inventory.database.Equipment
 
 class EquipmentGen:Fragment() {
+
+    private lateinit var equipment:Equipment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        equipment= Equipment(serialNumber = 0,equipmentName = "")
+
+
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_equipment,container,false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
     
